d3fedb6588fafaba382e16f180cd4d4f1f9a3a75
Abdun Noor Shihab <116369611+abdunnoorshihab@users.noreply.github.com>
2023-12-02 12:18:15 +0600

    first commit

---
+++ response.js
@@ -0,0 +1,12 @@
+const responseObj = {
+    hello: "Hey ! How are you doing ?",
+    hey: "Hey! What's Up",
+    date: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    fever:"Paracetamol",
+    cold:"acetaminophen or ibuprofen",
+    diabetes:"Insuline, Comete",
+    als:"Riluzole and edaravone",
+    arthritis:"Biologics, Analgesics",
+
+}
